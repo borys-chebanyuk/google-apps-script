540043d0c052e571d90cf4e4680bc3df8d434061
Borys Chebanyuk <chebanyuk25@gmail.com>
2025-11-25 23:22:26 +0200

    Update PDF_sender.js

--- PDF and Email from Sheet/PDF_sender.js
+++ PDF and Email from Sheet/PDF_sender.js
@@ -1,28 +1,31 @@
 function pdfMaker() {
-  const id = '1D_YwWmtmIUZRHHV0etybQ8yVjfT4NrZY8AkN7_AiTVk';
+  const id = 'YOUR_SHEET_ID';
   const ss = SpreadsheetApp.openById(id).getSheetByName('data');
-  const data = ss.getDataRange().getValues().slice(1); //Получаем весь диапазон данных и превращаем его в массив
-  //.slice(1) убирает первую строку (заголовки)
+  const data = ss.getDataRange().getValues().slice(1); //Get the entire data range and turn it into an array
+  //.slice(1) - removes the first line (headings)
 
-  //Перебираем каждую строку массива данных
+  //Next, we iterate through each row of the data array
   data.forEach((row, ind)=>{ 
-    let html = `<h1>${row[0]} ${row[1]}</h1>`; //cоздаём HTML-заголовок с именем и фамилией
-    html += `<div>Hello, is your company ${row[2]}?</div>`; //Добавляем строку с названием компании
-    html += `<div>Thank You</div>`; //Добавляем финальный текст
+    let html = `<h1>${row[0]} ${row[1]}</h1>`; //Create an HTML header with the first and last name
+    html += `<div>Hello, is your company ${row[2]}?</div>`; //Add a line with the company name
+    html += `<div>Thank You</div>`; //Adding the final text
     
-    const blob = Utilities.newBlob(html,MimeType.HTML); //Utilities.newBlob(content, contentType, name) — создаёт объект Blob (Создаём Blob (файл в памяти Google Apps Script) из HTML-строки) //MIME-тип — HTML
-    blob.setName(`${row[0]} ${row[1]}.pdf`); //Задаём имя будущему PDF файлу
+    const blob = Utilities.newBlob(html,MimeType.HTML); 
+    //Utilities.newBlob(content, contentType, name) — Creates a Blob object (Creating a Blob (a file in Google Apps Script memory) from an HTML string)
+    //MIME— HTML type
+    blob.setName(`${row[0]} ${row[1]}.pdf`); //We assign a name to the future PDF file
 
-    const email = row[3]; //e-mail получателя
-    const subject = `New PDF ${row[0]} ${row[1]}`; //Формируем тему письма
+    const email = row[3]; //recipient's e-mail
+    const subject = `New PDF ${row[0]} ${row[1]}`; //Forming the subject of the letter
     MailApp.sendEmail({
       to:email,
       subject:subject,
       htmlBody:html,
-      attachments:[blob.getAs(MimeType.PDF)], // конвертирует содержимое в PDF
-    }); //Отправляем письмо
+      attachments:[blob.getAs(MimeType.PDF)], // converts content to PDF
+    }); //Sending an email
 
-    const upRange = ss.getRange(ind+2,5).setValue('sent'); //Записываем в 5-й столбец статус sent
+    const upRange = ss.getRange(ind+2,5).setValue('sent'); //We write the sent status in the 5th column.
     Logger.log(blob);
     })
 }
+
